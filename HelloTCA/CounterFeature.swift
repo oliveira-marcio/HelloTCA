6ceb0c4db6fc05a6488c719b955ad550317c9d4b
Márcio Souza de Oliveira <marciosouzadev@gmail.com>
2023-07-11 18:33:43 +0100

    feat: Add effect to perform network request and action to handle response

--- HelloTCA/CounterFeature.swift
+++ HelloTCA/CounterFeature.swift
@@ -5,6 +5,7 @@
 //  Created by Márcio Oliveira on 11/07/2023.
 //
 
+import Foundation
 import ComposableArchitecture
 
 struct CounterFeature: ReducerProtocol {
@@ -18,6 +19,7 @@ struct CounterFeature: ReducerProtocol {
         case decrementButtonTapped
         case incrementButtonTapped
         case factButtonTapped
+        case factResponse(String)
     }
 
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
@@ -26,14 +28,26 @@ struct CounterFeature: ReducerProtocol {
             state.count -= 1
             state.fact = nil
             return .none
+
         case .incrementButtonTapped:
             state.count += 1
             state.fact = nil
             return .none
+
         case .factButtonTapped:
             state.fact = nil
             state.isLoading = true
-            return .none
+            return .run { [count = state.count] send in
+                let (data, _) = try await URLSession.shared
+                    .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                let fact = String(decoding: data, as: UTF8.self)
+                await send(.factResponse(fact))
+            }
+
+        case let .factResponse(fact):
+              state.fact = fact
+              state.isLoading = false
+              return .none
         }
     }
 }
