9b21d6c734ea5706fd5754bd3f44c01e2ae5737b
Márcio Souza de Oliveira <marciosouzadev@gmail.com>
2023-07-11 14:25:55 +0100

    feat: Create project with TCA

---
+++ HelloTCA/HelloTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTCAApp.swift
+//  HelloTCA
+//
+//  Created by Márcio Oliveira on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
